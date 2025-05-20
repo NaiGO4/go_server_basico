80acd722dbc75b9f2a116e7513f81710d8ad5276
Giancarlos Yanayaco <giancarlosyc04@gmail.com>
2025-05-19 21:43:20 -0500

    Crecion de los las conexiones de las rutas

--- main.go
+++ main.go
@@ -7,10 +7,27 @@ import (
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprint(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Adress = %s\n", address)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 no encontrada", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Metodo no soportado", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hola desde el servidor\n")
 
 }
 
